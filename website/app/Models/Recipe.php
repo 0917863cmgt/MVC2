a1010554639363006d533fd0e26ad5fd1f97b7b1
0917863cmgt <0917863cmgt@hr.nl>
2021-12-05 21:14:29 +0100

    Added model classes with relationship methods

---
+++ website/app/Models/Recipe.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'image',
+        'amount_people',
+        'ingredients',
+        'steps',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
+
+    public function author(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class, 'category_recipe');
+    }
+
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+}
