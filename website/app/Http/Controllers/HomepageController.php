c1a5753ace3bc9011d25b0325056dc9de64da059
0917863cmgt <0917863cmgt@hr.nl>
2021-12-06 11:43:42 +0100

    Added database queries to HomepageController for the recipes and Categories, added new html layout and component for recipe-cards, added new route for recipe-overview template

--- website/app/Http/Controllers/HomepageController.php
+++ website/app/Http/Controllers/HomepageController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 
 class HomepageController extends Controller
 {
     public function index(){
-        return view('homepage.index');
+
+        return view('homepage.index', [
+            'recipes' => Recipe::all(),
+            'parentCategories' => Category::where('isParent', true),
+            'cuisine' => Category::where('parent_id', 3),
+            'course' => Category::where('parent_id', 4),
+            'protein' => Category::where('parent_id', 1),
+            'vegetables' => Category::where('parent_id', 2),
+        ]);
     }
 }
--- website/resources/views/homepage/index.blade.php
+++ website/resources/views/homepage/index.blade.php
@@ -1,6 +1,18 @@
 @extends('components.layout')
 @section('content')
     <body id="homepage">
-        <h1>New beginnings</h1>
+        <div class="container-fluid px-5">
+            <div class="row gx-2">
+               <div class="col-12">
+                   <h1>Recipes</h1>
+
+                   <div class="row">
+                       @foreach($recipes as $recipe)
+                           <x-recipe-card :recipe="$recipe"></x-recipe-card>
+                       @endforeach
+                   </div>
+               </div>
+            </div>
+        </div>
     </body>
 @endsection
