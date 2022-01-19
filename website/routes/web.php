51d4697d056cd1244c4a02790dc8ad2f4990dc97
0917863cmgt <0917863cmgt@hr.nl>
2022-01-19 21:45:30 +0100

    Added new admin page routes and reworked some paths for better structure

--- website/routes/web.php
+++ website/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminRecipeController;
+use App\Http\Controllers\AdminUserController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
@@ -20,14 +23,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [HomepageController::class, 'index']);
-Route::get('/create', [RecipeController::class, 'store']);
-Route::get('/recipe/{recipe:slug}', [RecipeController::class, 'index']);
-Route::post('/recipe/{recipe:slug}/create', [LikeController::class, 'create']);
+Route::get('/create', [RecipeController::class, 'create'])->middleware('likes');
+Route::post('/create', [RecipeController::class, 'store'])->middleware('likes');
+Route::get('/recipe/{recipe:slug}', [RecipeController::class, 'show']);
+Route::post('/recipe/{recipe:slug}/create', [LikeController::class, 'store']);
 Route::post('/recipe/{recipe:slug}/destroy/{like:id}', [LikeController::class, 'destroy']);
 
-Route::get('/favourites', [LikeController::class, 'show'])->middleware('auth');
+Route::get('/favourites', [LikeController::class, 'index'])->middleware('auth');
 Route::get('/user-details', [UserController::class, 'show'])->middleware('auth');
 Route::get('/user-details/edit', [UserController::class, 'edit'])->middleware('auth');
+Route::patch('/user-details/update/{user}', [UserController::class, 'update'])->middleware('auth');
 
 Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store'])->middleware('guest');
@@ -36,3 +41,18 @@ Route::get('/register', [RegisterController::class, 'create']);
 Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
 
 Route::get('/logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::get('/admin', [AdminController::class, 'index'])->middleware('admin');
+Route::get('/admin/recipes/', [AdminRecipeController::class, 'index'])->middleware('admin');
+Route::get('/admin/recipes/create', [AdminRecipeController::class, 'create'])->middleware('admin');
+Route::post('/admin/recipes/create', [AdminRecipeController::class, 'store'])->middleware('admin');
+Route::get('/admin/recipes/edit/{recipe:slug}', [AdminRecipeController::class, 'edit'])->middleware('admin');
+Route::patch('/admin/recipes/update/{recipe:slug}', [AdminRecipeController::class, 'update'])->middleware('admin');
+Route::delete('/admin/recipes/delete/{recipe:slug}', [AdminRecipeController::class, 'destroy'])->middleware('admin');
+
+Route::get('/admin/users', [AdminUserController::class, 'index'])->middleware('admin');
+Route::get('/admin/users/create', [AdminUserController::class, 'create'])->middleware('admin');
+Route::post('/admin/users/create', [AdminUserController::class, 'store'])->middleware('admin');
+Route::get('/admin/users/edit/{user:username}', [AdminUserController::class, 'edit'])->middleware('admin');
+Route::patch('/admin/users/update/{user:username}', [AdminUserController::class, 'update'])->middleware('admin');
+Route::delete('/admin/users/delete/{user:username}', [AdminUserController::class, 'destroy'])->middleware('admin');
