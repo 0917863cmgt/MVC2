32c941d0ae8d376530a02c5dd28f1f7e5217e71f
0917863cmgt <0917863cmgt@hr.nl>
2022-01-21 15:13:12 +0100

    Added filtering functionality for multiple category types

--- website/app/Http/Controllers/HomepageController.php
+++ website/app/Http/Controllers/HomepageController.php
@@ -11,7 +11,7 @@ class HomepageController extends Controller
     public function index(){
 
         return view('homepage.index', [
-            'recipes' => Recipe::latest()->where('published', '=', 1)->filter(request(['search', 'category', 'author']))->paginate(8)->withQueryString(),
+            'recipes' => Recipe::latest()->where('published', '=', 1)->filter(request(['search', 'protein', 'vegetables', 'cuisine', 'course', 'author']))->paginate(8)->withQueryString(),
             'parentCategories' => Category::where('is_parent', true),
             'cuisine' => Category::where('parent_id', 3),
             'course' => Category::where('parent_id', 4),
--- website/resources/views/components/searchbar.blade.php
+++ website/resources/views/components/searchbar.blade.php
@@ -1,8 +1,17 @@
 <div style="max-width: 200px;margin: 0;">
     <form method="GET" action="#">
-        @if(request('category'))
-            <input type="hidden" name="category" placeholder="" value="{{request('category')}}">
+        @if(request('protein'))
+            <input type="hidden" name="category" placeholder="" value="{{request('protein')}}">
         @endif
+            @if(request('vegetables'))
+                <input type="hidden" name="category" placeholder="" value="{{request('vegetables')}}">
+            @endif
+            @if(request('cuisine'))
+                <input type="hidden" name="category" placeholder="" value="{{request('cuisine')}}">
+            @endif
+            @if(request('course'))
+                <input type="hidden" name="category" placeholder="" value="{{request('course')}}">
+            @endif
         <input type="text" name="search" placeholder="Search..." class="article-searchbar">
     </form>
 </div>
