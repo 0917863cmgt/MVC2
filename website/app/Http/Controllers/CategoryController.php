6d2cb315d0728bff8695242da1a064b443c0238d
0917863cmgt <0917863cmgt@hr.nl>
2022-01-23 23:07:22 +0100

    Updated stupid category controller where min:4 changed to 3

--- website/app/Http/Controllers/CategoryController.php
+++ website/app/Http/Controllers/CategoryController.php
@@ -47,8 +47,8 @@ class CategoryController extends Controller
         $attributes = request()->validate([
             'is_parent' => 'required|integer|min:0|max:1',
             'parent_id' => 'integer|min:1',
-            'name' => [Rule::unique('categories', 'name'), 'string', 'min:4' ],
-            'slug' => [Rule::unique('categories', 'slug'), 'string', 'min:4' ]
+            'name' => [Rule::unique('categories', 'name'), 'string', 'min:3' ],
+            'slug' => [Rule::unique('categories', 'slug'), 'string', 'min:3' ]
         ]);
 
         Category::create($attributes);
@@ -92,8 +92,8 @@ class CategoryController extends Controller
         $attributes = request()->validate([
             'is_parent' => 'required|integer|min:0|max:1',
             'parent_id' => 'integer|min:1',
-            'name' => [Rule::unique('categories', 'name')->ignore($category->id), 'string', 'min:4' ],
-            'slug' => [Rule::unique('categories', 'slug')->ignore($category->id), 'string', 'min:4' ]
+            'name' => [Rule::unique('categories', 'name')->ignore($category->id), 'string', 'min:3' ],
+            'slug' => [Rule::unique('categories', 'slug')->ignore($category->id), 'string', 'min:3' ]
         ]);
 
         if($attributes['parent_id'] == null && $attributes['is_parent'] == 1){
