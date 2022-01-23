c2716f6104d843744322f5c8b87636f94a7d1c81
0917863cmgt <0917863cmgt@hr.nl>
2022-01-24 00:05:02 +0100

    recipe card max and min height changed

--- website/resources/views/components/recipe/recipe-card.blade.php
+++ website/resources/views/components/recipe/recipe-card.blade.php
@@ -1,5 +1,5 @@
 @props(['recipe'])
-<div class="col-3">
+<div style="min-height:602px;max-height: 602px;" class="col-3">
     <a href="/recipe/{{$recipe->slug}}" style="text-decoration: none;color:inherit; max-width: inherit">
         <img src="{{asset('storage/' . $recipe->image) ?: $recipe->image }}" style="max-width: inherit">
         <h3>{{Illuminate\Support\Str::limit($recipe->title,35,"...")}}</h3>
